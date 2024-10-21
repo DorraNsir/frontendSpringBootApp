c8997299cd33e165445bddc52bf7cac84503048a
DorraNsir <dorransir24@gmail.com>
2024-10-21 21:39:31 +0200

    create film model class

--- ProduitFilm/src/app/films/films.component.ts
+++ ProduitFilm/src/app/films/films.component.ts
@@ -1,25 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import { Film } from '../model/film.model';
 
 @Component({
   selector: 'app-films',
   templateUrl: './films.component.html'
 })
 export class FilmsComponent implements OnInit {
-  films!:string[];
-  constructor(){
-    this.films = [
-      "Inception",
-      "The Dark Knight",
-      "Interstellar",
-      "Parasite",
-      "The Matrix",
-      "The Lord of the Rings",
-      "Pulp Fiction",
-      "The Godfather",
-      "Forrest Gump",
-      "Fight Club"
+  films?:Film[];
+  constructor() {
+    this.films = [ 
+      {idFilm : 1, nomFilm : "Inception", prixFilm : 15.50, dateCreation : new Date("07/16/2010")},
+      {idFilm : 2, nomFilm : "Interstellar", prixFilm : 18.00, dateCreation : new Date("11/07/2014")},
+      {idFilm : 3, nomFilm : "The Matrix", prixFilm : 12.75, dateCreation : new Date("03/31/1999")}
     ];
   }
+  
   ngOnInit(): void {
       
   }
